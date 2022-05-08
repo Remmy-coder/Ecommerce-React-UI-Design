17bace87b6964a8345fa188b0a1d9c7c413ab197
const devilMyOpp <54766477+Remmy-coder@users.noreply.github.com>
2022-05-08 22:13:15 +0100

    Commit

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.pinimg.com/564x/2c/52/6c/2c526c76ee693a2b4d564bd744da4d94.jpg",
+        title: "LOOKING EXOTIC",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://i.pinimg.com/564x/ed/ec/bd/edecbdee72bc8dbc8eb10e4819b53d2f.jpg",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+    },
+    {
+        id: 3,
+        img: "https://i.pinimg.com/564x/fe/db/f5/fedbf539e674a8139d9d8728e9cd2a17.jpg",
+        title: "LOUNGEWEAR LOVE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://i.pinimg.com/564x/a0/71/29/a071291b6bf8586e3a22933b474e891e.jpg",
+        title: "SHIRT STYLE",
+    },
+    {
+        id: 2,
+        img: "https://i.pinimg.com/564x/2f/89/0b/2f890b65a02eefd14206127bdf3161e1.jpg",
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "https://i.pinimg.com/564x/2f/cd/2d/2fcd2d53503f04ef8cda34d5cc0a4de0.jpg",
+        title: "LIGHT JACKETS",
+    },
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://i.pinimg.com/564x/c7/6a/f2/c76af2238e80f39d39c7cdda5fed967c.jpg",
+    },
+    {
+        id: 2,
+        img: "https://i.pinimg.com/564x/67/98/51/6798518353e65c7e2b2efb49837630e3.jpg",
+    },
+    {
+        id: 3,
+        img: "https://i.pinimg.com/564x/3d/cf/5d/3dcf5da36b4f0894bf3a7222cfcdfd35.jpg",
+    },
+    {
+        id: 4,
+        img: "https://i.pinimg.com/564x/21/98/05/219805831143c8050be593f49a03d04e.jpg",
+    },
+    {
+        id: 5,
+        img: "https://i.pinimg.com/564x/d9/1b/52/d91b52ca7d13c354b4bde62b863e62e0.jpg",
+    },
+    {
+        id: 6,
+        img: "https://i.pinimg.com/564x/03/07/3c/03073c22baf4407e5dd040b2979e39bf.jpg",
+    },
+    {
+        id: 7,
+        img: "https://i.pinimg.com/564x/20/ce/24/20ce24c9a9071fc73e82dfe7a90d62c8.jpg",
+    },
+    {
+        id: 8,
+        img: "https://i.pinimg.com/564x/d4/45/8c/d4458c7adb0749199320b4abab14d023.jpg",
+    },
+]
